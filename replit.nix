e1334645e00d7cd1dc62d18673bbcc46f1927991
Shahnawaz Ali <43275155-jejdetrs@users.noreply.replit.com>
2025-05-29 18:26:32 +0000

    Post-Recovery checkpoint

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-  deps = [
-    pkgs.python311
-    pkgs.python311Packages.pip
-    pkgs.python311Packages.flask
-    pkgs.python311Packages.flask_cors
-    pkgs.python311Packages.openai
-  ];
-}
