a1844b67f21efe9a03f97aceaa57c92efd1dacc9
SirShahnawazAli <jejdetrs@gmail.com>
2025-05-30 17:06:56 +0000

    Allow requests from DeepSite (CORS fix)

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://*.replit.dev"}})
+CORS(app)
+
 
 
 import os
